3a38149e13428739aa9e1929c8633874e614a60f
Amish Maqbool Khan <amishmaqbool@gmail.com>
2025-06-27 01:36:01 +0500

    adds

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import requestIp from "request-ip";
 import "dotenv/config";
 import cros from "cors";
+import path from "path";
 
 // const MONGODB_URL = `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASS}mymongoinit.6md0cxy.mongodb.net/gemini?retryWrites=true&w=majority`;
 const MONGODB_URL = process.env.MONGO_URL; //"mongodb://localhost:27017/"
@@ -28,6 +29,14 @@ import authRoutes from "./router/auth.js";
 
 app.use("/gemini", publicRoutes);
 app.use(authRoutes);
+
+// Serve static files from the React app build at /chatapp
+app.use("/chatapp", express.static(path.join(__dirname, "../../client-fe/build")));
+
+// Catch-all handler to serve React's index.html for any non-API route under /chatapp
+app.get("/chatapp/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../client-fe/build", "index.html"));
+});
 
 app.use((error, req, res, next) => {
   console.log(error);
